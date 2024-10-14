5a92718403f88a253640a7305365895de24993f7
PeterCreely <pcreely@hotmail.com>
2024-10-14 16:28:15 +0100

    Update scriptie.js

--- scriptie.js
+++ scriptie.js
@@ -305,7 +305,7 @@ const handleGuess = (letter, button) => {
                 }
             });
             lostMessage.innerHTML = `<strong>Ó Níl!<br>Rith tú amach as buille faoi thuairim</strong><br><br>Tá ${maxLives} shaol fágtha agat. <br><br> Bhí na focail:<br>${associatedWordsMessage} <br>`;
-            continueButton.innerText = `Lean ar aghaidh le ${maxLives} shaol fágtha`; // Set button text
+   //         continueButton.innerText = `Lean ar aghaidh le ${maxLives} shaol fágtha`; // Set button text
             lostModal.style.display = 'block';
 
             points--;
